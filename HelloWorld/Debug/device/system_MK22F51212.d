529c5402fb701879c4b86cc816668c4d60066526
florianbaer <florian.baer@stud.hslu.ch>
2020-03-07 11:39:29 +0100

    inital commit

---
+++ HelloWorld/Debug/device/system_MK22F51212.d
@@ -0,0 +1,24 @@
+device/system_MK22F51212.o device/system_MK22F51212.d: \
+ ../device/system_MK22F51212.c ../device/fsl_device_registers.h \
+ ../device/MK22F51212.h \
+ /home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/core_cm4.h \
+ /home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/cmsis_version.h \
+ /home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/cmsis_compiler.h \
+ /home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/cmsis_gcc.h \
+ ../device/system_MK22F51212.h ../device/MK22F51212_features.h
+
+../device/fsl_device_registers.h:
+
+../device/MK22F51212.h:
+
+/home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/core_cm4.h:
+
+/home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/cmsis_version.h:
+
+/home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/cmsis_compiler.h:
+
+/home/hslu/MCUXpresso_workspace/HelloWorld/CMSIS/cmsis_gcc.h:
+
+../device/system_MK22F51212.h:
+
+../device/MK22F51212_features.h:
